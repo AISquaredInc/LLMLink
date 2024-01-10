d7eacd45560d9057bad098fbe467f1c96aeafac1
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-01-10 11:03:24 -0500

    fixing issue with naming
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- llmlink/model/__init__.py
+++ llmlink/model/__init__.py
@@ -1,6 +1,6 @@
 """
-Chainlink.model is a subpackage of the Chainlink package which focuses on building
-the models to be used in Chainlink applications
+LLMLink.model is a subpackage of the LLMLink package which focuses on building
+the models to be used in LLMLink applications
 """
 from .Model import Model
 from .Agent import Agent
